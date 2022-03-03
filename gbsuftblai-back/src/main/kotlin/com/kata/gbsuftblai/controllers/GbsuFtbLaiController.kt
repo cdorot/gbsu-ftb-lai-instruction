82e00210f0ed6d40b7df1fbbd2660a4c3074f855
Cédric DOROT <cdorot@ippon.fr>
2022-03-03 12:21:49 +0100

    Move inner class in a view package

--- gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/controllers/GbsuFtbLaiController.kt
+++ gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/controllers/GbsuFtbLaiController.kt
@@ -1,19 +1,20 @@
 package com.kata.gbsuftblai.controllers
 
 import com.kata.gbsuftblai.services.GbsuFtbLaiService
+import com.kata.gbsuftblai.views.ResultDto
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/gbsu-ftb-lai")
 class GbsuFtbLaiController(private val gbsuFtbLaiService: GbsuFtbLaiService) {
 
     @GetMapping("/{inputNumber}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    @CrossOrigin(origins = ["http://localhost:8080", "http://localhost:4200"])
     fun convertNumber(@PathVariable(name = "inputNumber") inputNumber: Int): ResultDto {
         return ResultDto(gbsuFtbLaiService.convertNumber(inputNumber))
     }
-
-    data class ResultDto(val result: String)
 
 }
---
+++ gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/views/ResultDto.kt
@@ -0,0 +1,3 @@
+package com.kata.gbsuftblai.views
+
+data class ResultDto(val result: String)
